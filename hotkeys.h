81f349e2064c0a6aa3a34925c6ddafe5f4069284
Hritik Vijay <hritikxx8@gmail.com>
2021-01-04 21:23:55 +0530

    stop quitting on me unexpectedly

--- hotkeys.h
+++ hotkeys.h
@@ -71,7 +71,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,             XK_BackSpace,      quit,           {0} },
 
 	// custom keybindings
 	{ WINKEY,			XK_l                    ,			spawn ,			{.v = lock}}         ,
